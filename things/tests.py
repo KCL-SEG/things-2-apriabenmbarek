1fbd0b1b7048ccd19bb8ce42b9b04cf23b0d9a07
Apria Benmbarek <apria.benmbarek@kcl.ac.uk>
2022-11-05 13:15:19 +0000

    Complete exercise

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from things.forms import ThingForm
+from things.models import Thing
 
-# Create your tests here.
+class ThingFormTestCase(TestCase):
+
+    def setUp(self):
+        self.form_input = {
+            'name': 'Apple',
+            'description': 'A round fruit.',
+            'quantity': '5'
+        }
+
+    def test_valid_thing_form(self):
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_has_necessary_fields(self):
+        form = ThingForm()
+        self.assertIn('name', form.fields)
+        self.assertIn('description', form.fields)
+        self.assertIn('quantity', form.fields)
